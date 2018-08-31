a514cd08b68cbf8d09c607c8111f3695717d829b
milde <milde@hs-fulda.de>
2018-08-31 09:40:56 +0200

    JSON printout added

--- csvReader.go
+++ csvReader.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
-	//       "math"
 	//      "runtime"
 )
+
+type event struct {
+	TimeStamp float64 `json:"timestamp,omitempty"`
+	C1        float64 `json:"c1,omitempty"`
+	C2        float64 `json:"c2,omitempty"`
+}
+
+type name struct {
+	v, n string
+}
 
 func main() {
 	fmt.Println("CSV Reader, JTM, 2018")
@@ -28,7 +39,6 @@ func main() {
 		// keine Dateien angegeben
 	} else {
 		for _, arg := range files {
-
 			f, err := os.Open(arg)
 
 			if err != nil {
@@ -47,10 +57,31 @@ func main() {
 func processLines(f *os.File) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
-		fmt.Println(input.Text())
+		//fmt.Println(input.Text())
 
-		for _, field := range strings.Split(string(input.Text()), ";") {
-			fmt.Println(field)
+		fields := strings.Split(string(input.Text()), ";")
+		values := make([]float64, len(fields))
+
+		i := 0
+		for _, field := range fields {
+			//fmt.Println(field)
+			sfield := strings.Replace(field, ",", ".", 1)
+
+			val, err := strconv.ParseFloat(sfield, 64)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			} else {
+				values[i] = val
+				i++
+			}
 		}
+
+		//ev := event(timeStamp: values[0], c1: values[1], c2: values[2])
+		ev := event{values[0], values[1], values[2]}
+
+		//nm := name{"Rebecca", "Milde"}
+		js, _ := json.Marshal(ev)
+		fmt.Printf("%s\n", js)
 	}
 }
